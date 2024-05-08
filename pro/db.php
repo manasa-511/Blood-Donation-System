a9af29431cf0804ca6c39ef7f1bb69ea6f945810
manasa-511 <167171985+manasa-511@users.noreply.github.com>
2024-05-08 19:20:19 +0530

    Delete pro directory

--- pro/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$con=mysqli_connect("localhost","root","","pro");
-if(!$con)
-{
-    die("Connection Error");
-}
-?>
--- pro/php/config.php
+++
@@ -1,5 +0,0 @@
-<?php 
- 
- $con = mysqli_connect("localhost","root","","pro") or die("Couldn't connect");
-
-?>
